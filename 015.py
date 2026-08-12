4fbd736adc95b4af89057dacd33c6aac04751353
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-12 14:47:57 -0500

    Adicionei Resposta!

--- 015.py
+++ 015.py
@@ -1,2 +1,27 @@
 #Resposta:
+#Estrutura dos dois objetos:
+#
+# 1) Objeto Concreto:
+# 
+# Cliente:
+# 
+# Atributos:
+# 
+# Nome: CPF: Valor_da_compra: Metodo_de_pagamento: Data: Hora:
+#
+# Metodos:
+#
+# Entregar_o_produto_comprado() Devolução() Reembolso() Desconto()
+#
+# 2) Objeto Abstrato:
+#
+# Sistema_de_aluguel:
+#
+# Atributos:
+#
+# Nome_do_cliente: data_de_retirada: data_de_entrega: tempo_usado: CPF:
+#
+# Metodos:
+#
+# ALugar() Lançar_novo_veiculo_no_sistema() Multa_por_atraso()
 #
