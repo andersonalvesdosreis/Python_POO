48a452d61831871cb08419d257ca1af330133216
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-12 14:51:18 -0500

    Adicionei Resposta!

--- 015.py
+++ 015.py
@@ -13,6 +13,10 @@
 #
 # Entregar_o_produto_comprado() Devolução() Reembolso() Desconto()
 #
+# Estado:
+#
+# Nome: Anderson  CPF: 123.456.789-00  Valor_da_compra: R$100,00  Metodo_de_pagamento: Debito  Data: 10/04  Hora: 11:30
+#
 # 2) Objeto Abstrato:
 #
 # Sistema_de_aluguel:
@@ -24,4 +28,8 @@
 # Metodos:
 #
 # ALugar() Lançar_novo_veiculo_no_sistema() Multa_por_atraso()
+#
+# Estado:
+#
+# Nome_do_cliente: Anderson  data_de_retirada: 02/05  data_de_entrega: 04/05  tempo_usado: 2dias  CPF: 123.456.789-00
 #
