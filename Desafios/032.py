1ff10fa15b30b7cbeec90aa5011659d88177a2b1
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-26 13:01:41 -0500

    Adicionei Resposta!

--- Desafios/032.py
+++ Desafios/032.py
@@ -1,3 +1,5 @@
+from rich import print
+import hashlib
 class ContaBancaria:
     """
     Classe Para Definir conta bancaria:
@@ -38,17 +40,64 @@ class ContaBancaria:
     Autor: Anderson Alves
     """
 
-    def __init__(self,nome, saldo = 0):
+    def __init__(self,nome, senha, saldo = 0):
         self.__nome = nome
         self.__saldo = saldo
+        self._senha = senha
+        mensagem_em_bytes = self._senha.encode('utf-8')
+        hash_objeto = hashlib.sha256(mensagem_em_bytes)
+        resultado = hash_objeto.hexdigest()
+        self.__senha_cript = resultado
+
+    def pedir_senha(self):
+        tentativa = str(input('Digite sua senha para confirmar: '))
+        mensagem_em_bytes = tentativa.encode('utf-8')
+        hash_objeto = hashlib.sha256(mensagem_em_bytes)
+        resultado = hash_objeto.hexdigest()
+        self._tentativa = resultado
+        if self._tentativa == self.__senha_cript:
+            return True
+        else:
+            return False
 
     def deposito_no_saldo(self, deposito = 0):
-        self.__saldo += deposito
+        if self.pedir_senha() == True:
+            self.__saldo += deposito
+        else:
+            return None
 
     def saque_no_saldo(self, saque = 0):
-        if saque <= self.__saldo:
-            self.__saldo -= saque
+        if self.pedir_senha == True:
+            if saque <= self.__saldo:
+                self.__saldo -= saque
+        else:
+             return None
 
     def __str__(self):
         return f"{'=-'*20} \n{'Nome:':<20} | {'Saldo:':<12} \n{self.__nome:<20} | R$ {self.__saldo:>9.2f}"
 
+print('=-'*20)
+
+pergunta = str(input('Qual seu nome? '))
+pergunta_senha = str(input('Digite sua senha: '))
+pergunta2 = float(input('Qual seu saldo? '))
+cliente2 = ContaBancaria(nome= pergunta,senha= pergunta_senha,saldo= pergunta2)
+print(cliente2)
+
+print('=-'*20)
+
+pergunta3 = str(input('Deseja depositar algum valor? (S= para sim)')).upper()
+if pergunta3 == 'S':
+    pergunta_valor = float(input('Quanto? '))
+    cliente2.deposito_no_saldo(deposito=pergunta_valor)
+    print(cliente2)
+pergunta4 = str(input('Deseja saquar algum valor? (S= para sim)')).upper()
+if pergunta4 == 'S':
+    pergunta_valor = float(input('Quanto? '))
+    cliente2.saque_no_saldo(saque=pergunta_valor)
+    print(cliente2)
+pergunta5 = str(input('Deseja ver estrutura do codigo? (S= para sim)')).upper()
+if pergunta4 == 'S':
+    print(ContaBancaria.__doc__)
+
+print('=-'*20)
