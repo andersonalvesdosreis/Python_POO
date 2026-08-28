1baa48507decf01f210c42834fe591d84cc350a5
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-28 15:52:04 -0500

    Add Estrutura!

--- Aula 14/testando_polimorfismo_p2.py
+++ Aula 14/testando_polimorfismo_p2.py
@@ -1,5 +1,24 @@
 from rich import print
-from abc import ABC , abstractmethod
+from functools import singledispatchmethod
 
 #Aula 14 --> Overloading
+
+class Analisador:
+
+    @singledispatchmethod
+    def analisador(self,vlr):
+        print(f'Não foi possivel analisar o valor {vlr}')
+
+    @analisador.register
+    def _(self,vlr:int):
+        print(f'{vlr} é um numero!')
+
+    @analisador.register
+    def _(self,vlr:str):
+        print(f'{vlr} é uma cadeia de caracteres!')
+
+obj = Analisador()
+obj.analisador(3)
+obj.analisador('Anderson')
+obj.analisador({'Idade':34})
 
