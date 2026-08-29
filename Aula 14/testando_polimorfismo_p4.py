3dfd976a4548d81fd9c8b3cc49421093a7a60165
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-29 09:42:53 -0500

    Add Resposta!

--- Aula 14/testando_polimorfismo_p4.py
+++ Aula 14/testando_polimorfismo_p4.py
@@ -1,7 +1,35 @@
 from rich import print
-from functools import singledispatchmethod
 
 #Aula 14 --> Overloading
 
-class Abrir:
-    
+class Porta:
+    def abrir(self):
+        print('Girar a massaneta e empurre a porta')
+
+class Empresa:
+    def abrir(self):
+        print('Cadastre um novo CNPJ')
+
+class Ovo:
+    def abrir(self):
+        print('Quebre a casca')
+
+class Pedra:
+    pass
+
+def tentar_abrir(obj):
+    try:
+        obj.abrir()
+    except:
+        print(f'Encotrei problemas ao tentar abrir {obj.__class__.__name__}')
+
+
+obj1 = Porta()
+obj2 = Empresa()
+obj3 = Ovo()
+obj4 = Pedra()
+
+tentar_abrir(obj1)
+tentar_abrir(obj2)
+tentar_abrir(obj3)
+tentar_abrir(obj4)
